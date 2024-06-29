fbd5843afec5b062eefa6f427fe53651082e83b2
rbgusdlza <rbgusdlza@gmail.com>
2024-06-30 03:16:16 +0900

    fix: static 관련 주석 제거

--- src/main/java/rbgusdlza/springlab/bean/auto/autowired/test2/TestBeanAuto2.java
+++ src/main/java/rbgusdlza/springlab/bean/auto/autowired/test2/TestBeanAuto2.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class TestBeanAuto2 {
 
-    //빈 자동 주입 받을 때, static으로 설정되어 있을 경우 자동 주입되지 않는다.
-    //static 영역은 클래스가 로딩 시점에 초기화되기 때문(빈 라이프 사이클과도 호환 x)
     private final ObjectMapper fooObject;
     private final ObjectMapper barObject;
 }
